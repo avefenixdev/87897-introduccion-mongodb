94335017149c8967bd09d5e443f7ad5439b65918
Ave Fenix Dev <avefenixdev@gmail.com>
2026-09-18 09:45:19 -0300

    Trabajando con cursores

--- clase-05/scripts/cursor01.js
+++ clase-05/scripts/cursor01.js
@@ -1,2 +1,23 @@
-print('Hola mundo!')
-console.log('Hola mundo!')
+print('Cursores')
+
+print('-------------------------------------------------------------')
+print('Creamos dinamicamente 100 documentos en la colección archivos')
+print('-------------------------------------------------------------')
+
+/* for (let i = 0; i < 100; i++) {
+    db.archivos.insertOne(
+        {
+            archivo_id: 1,
+            nombre: 'archivo_' + i,
+            created_at: new Date()
+        }
+    )
+} */
+
+print('------------------------------')
+print('Interactuando con los cursores')
+print('------------------------------')
+
+let cursor = db.archivos.find()
+//print(cursor) // <---- El cursor es un objeto
+//console.dir(cursor)
